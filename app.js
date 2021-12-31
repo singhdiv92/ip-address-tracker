6a2b805dec293176332804856838e048ae21d853
Divya Singh <singhdiv92@gmail.com>
2021-12-31 12:07:29 +0530

    second commit

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.post("/", function (req, res) {
 
 
 
-app.listen(3000,()=>console.log("server running at port 3000"));
+app.listen(process.env.PORT||3000,()=>console.log("server running at port 3000"));
 
 
